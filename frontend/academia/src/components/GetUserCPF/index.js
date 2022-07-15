482c31121bba93739e95c61d53cc09df28e3f7a9
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-07-15 18:51:32 -0300

    serach

--- frontend/academia/src/components/GetUserCPF/index.js
+++ frontend/academia/src/components/GetUserCPF/index.js
@@ -5,56 +5,85 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const GetUserCPF = () => {
   const [userCPF, setCPF] = useState("");
-  const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const [notFound, setNotFound] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   const cpfContent = (event) => {
     setCPF(event.target.value);
   };
 
-  const seachError = (state) => {
-    setError(state);
-    setSuccess(!state);
-    setNotFound(!state);
-  };
-
   const searchSuccess = (state) => {
-    setError(!state);
     setSuccess(state);
     setNotFound(!state);
   };
 
   const searchNotFound = (state) => {
-    setError(!state);
+    setOpen(notFound);
     setSuccess(!state);
     setNotFound(state);
+    setUser(false);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <>
-      <Grid container spacing={2}>
-        {
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {"Usuário não encontrado"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Não foi possível localizar o usuário com CPF {userCPF}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Grid container spacing={0.5} justifyContent="center">
+        {search && (
           <GetCPF
             cpf={userCPF}
-            setError={seachError}
             setSuccess={searchSuccess}
             setNotFound={searchNotFound}
             setLoadng={setLoading}
             setUser={setUser}
+            search={search}
+            setSearch={setSearch}
           />
-        }
+        )}
         <Grid item>
           <Box
             component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "25ch" },
-            }}
+            sx={{ display: "flex" }}
             noValidate
             autoComplete="off"
           >
@@ -64,14 +93,47 @@ const GetUserCPF = () => {
               variant="filled"
               onChange={cpfContent}
             />
+            <Box sx={{ display: "flex" }}>
+              {loading && <CircularProgress />}
+            </Box>
           </Box>
         </Grid>
         <Grid item>
-          <Button variant="contained" disableElevation onClick={() => search()}>
-            Buscar
-          </Button>
+          <Box sx={{ display: "flex" }}>
+            <Button
+              variant="contained"
+              disableElevation
+              onClick={() => setSearch(true)}
+            >
+              Buscar
+            </Button>
+          </Box>
         </Grid>
       </Grid>
+      {user && (
+        <Grid container spacing={0.5} justifyContent="center">
+          <Grid item>
+            {
+              <TextField
+                id="standard-basic"
+                label="ID"
+                variant="standard"
+                value={user.id}
+              />
+            }
+          </Grid>
+          <Grid item>
+            {
+              <TextField
+                id="standard-basic"
+                label="Name"
+                variant="standard"
+                value={user.name}
+              />
+            }
+          </Grid>
+        </Grid>
+      )}
     </>
   );
 };
