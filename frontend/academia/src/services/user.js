5d2f54ccb1c31cc0c2187d1b998074ad3ece912f
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-07-15 12:28:46 -0300

    adjusting the bug the .env

--- frontend/academia/src/services/user.js
+++ frontend/academia/src/services/user.js
@@ -3,6 +3,6 @@ import jwt_decode from "jwt-decode";
 
 export const login = async (body) => {
   const { data } = await api.post("/user/login", body);
-  console.log(jwt_decode(data.jwt));
+
   return jwt_decode(data.jwt);
 };
