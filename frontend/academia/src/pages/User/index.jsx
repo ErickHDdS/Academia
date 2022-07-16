5cdf8307859fb8094522db005d90d16821362282
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-07-15 23:00:24 -0300

    create a get workouts
    Co-authored-by: Tarcísio Prates <20193008761@aluno.cefetmg.br>
    Co-authored-by: Tarcísio Prates <tarcisiobatistaprates@gmail.com>

--- frontend/academia/src/pages/User/index.jsx
+++ frontend/academia/src/pages/User/index.jsx
@@ -1,21 +1,94 @@
-import PhysicalExam from "../../components/PhysicalExam";
-import Trainig from "../../components/Training";
+import { useEffect } from "react";
+import GetUserCPF from "../../components/GetUserCPF";
+import { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import { Input, FormHelperText } from "@mui/material";
+import { getWorkouts } from "../../services/user";
 
 import "./style.css";
 
 export default function User() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [fatPercentage, setFatPercentage] = useState("");
+  const [massPercentage, setMassPercentage] = useState("");
+  const [imc, setImc] = useState("");
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    async function handleGetWorkout() {
+      const data = await getWorkouts();
+      setWorkouts(data);
+    }
+
+    async function handleGetExam() {
+      // const data = await getExam(window.localStorage.getItem("userId"));
+      // console.log(data);
+      // setWeight(data.weight);
+      // setHeight(data.height);
+      // setPressure(data.pressure);
+      // setFatPercentage(data.fatPercentage);
+      // setMassPercentage(data.massPercentage);
+      // setImc(data.imc);
+    }
+    handleGetWorkout();
+    handleGetExam();
+  }, []);
+
   return (
     <section className="user-main">
-      <h2 className="user-title">Meus dados</h2>
-      
+      <h1 className="user-title">Informações</h1>
+
       <div className="data-doctor">
         <h3>Dados médicos</h3>
-        <PhysicalExam />
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={weight} />
+          <FormHelperText>Peso (kg)</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={height} />
+          <FormHelperText>Altura (m)</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={pressure} />
+          <FormHelperText>Pressão Arterial (mmHg)</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={fatPercentage} />
+          <FormHelperText>Percentual de gordura no corpo (%)</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={massPercentage} />
+          <FormHelperText>Percentual de massa magra (%)</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+          <Input readOnly="true" value={imc} />
+          <FormHelperText>IMC</FormHelperText>
+        </FormControl>
       </div>
 
       <div className="data-training">
         <h3>Meu treino</h3>
-        <Trainig />
+
+        {workouts.map((training) => (
+          <div>
+            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+              <Input readOnly="true" value={training.name} />
+              <FormHelperText>Exercício/Músculo</FormHelperText>
+            </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "25ch" }}>
+              <Input readOnly="true" value={training.repetitions} />
+              <FormHelperText>Repetições</FormHelperText>
+            </FormControl>
+          </div>
+        ))}
       </div>
     </section>
   );
